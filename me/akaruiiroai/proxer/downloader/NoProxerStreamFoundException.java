53b789c3d44ac6e02f3da7747ee715bd98c236aa
Akarui Iroai <33889344+akaruiiroai@users.noreply.github.com>
2017-11-22 07:40:08 +0100

    Thrown when no ProxerHDStream is found

---
+++ me/akaruiiroai/proxer/downloader/NoProxerStreamFoundException.java
@@ -0,0 +1,14 @@
+package me.akaruiiroai.proxer.downloader;
+
+public class NoProxerStreamFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public NoProxerStreamFoundException() {
+	}
+
+	public NoProxerStreamFoundException(String message) {
+		super(message);
+	}
+	
+}
